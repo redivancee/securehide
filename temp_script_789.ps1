473913567e8b1a05fe0754a4b3c8256494b6bdd0
redivancee <diorhumber11@gmail.com>
2025-03-11 17:49:15 -0700

    Update temp_script_789.ps1

--- temp_script_789.ps1
+++ temp_script_789.ps1
@@ -1,10 +1,10 @@
 # ==============================
 # Auto-Updater (Checks GitHub)
 # ==============================
-# Version: 1.00 Demo 3
+# Version: 1.02
 $localScriptPath = $MyInvocation.MyCommand.Path
 $repoUrl = "https://raw.githubusercontent.com/redivancee/securehide/main/temp_script_789.ps1"
-$localVersion = "1.00 Demo 3"  # Change this when you upload new versions to GitHub!
+$localVersion = "1.02"  # Change this when you upload new versions to GitHub!
 
 try {
     $remoteScript = Invoke-WebRequest -Uri $repoUrl -UseBasicParsing -ErrorAction Stop
